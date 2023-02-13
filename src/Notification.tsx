b6cdfbc41616c6e945f6cefabacfcccae8e8187d
Christopher Koch <chris@koch.codes>
2023-02-13 16:38:42 +0100

    Update Notification.tsx

--- src/Notification.tsx
+++ src/Notification.tsx
@@ -54,7 +54,7 @@ interface PopupProps {
 
 const Notification: React.FC<PopupProps> = ({ message, closePopup, open }) => {
 
-    setTimeout(closePopup, 1000000);
+    setTimeout(closePopup, 5000);
     return (
         <Box className={open ? "open" : "closed"}>
             <Icon><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M21 7L9 19l-5.5-5.5l1.41-1.41L9 16.17L19.59 5.59L21 7Z" /></svg></Icon>
